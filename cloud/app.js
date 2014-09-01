589989c8dc2d19d268d9d686692a833e47b02f67
Jatin Pandey <jatinp@friendsad-lm.(none)>
2014-08-31 23:21:30 -0700

    Form functionality

--- cloud/app.js
+++ cloud/app.js
@@ -6,7 +6,12 @@ var app = express();
 // Global app configuration section
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
+
+app.use(express.basicAuth());
 app.use(express.bodyParser());    // Middleware for reading request body
+app.use(express.cookieParser());
+app.use(express.cookieSession());
+app.use(express.csrf());
 
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
